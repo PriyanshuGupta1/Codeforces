9991aa9fb3090f7c17c812f6b428ee30ad197788
PriyanshuGupta1 <priyanshu24jan@gmail.com>
2022-03-04 17:23:41 +0530

    743

---
+++ 743/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--) 
+    {
+        ll n;
+        cin>>n;
+        string a;
+        cin>>a;
+        while(a.)
+        
+    }
+    return 0;
+}
